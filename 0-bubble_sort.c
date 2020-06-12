69628e0ec01062a9b8a64cd578ea72928be7bccf
andreshugueth <andreshugueth@gmail.com>
2020-06-12 16:24:44 -0500

    fixing condition

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,7 +13,9 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int swap;
 
-	swap = 0;
+	if (!array || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
