06b727e5a93333a8143a58cc18a4467adfca2ee5
andreshugueth <andreshugueth@gmail.com>
2020-06-12 00:14:40 -0500

    header uploaded

--- sort.h
+++ sort.h
@@ -1,6 +1,8 @@
 #ifndef _SORT_H_
 #define _SORT_H_
 
+#include <stddef.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -18,4 +20,5 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+
 #endif
